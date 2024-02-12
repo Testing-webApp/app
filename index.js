29f62c8126e911e8e84a7ce0c5735166d6a302ee
nikola <nicisekulovski@gmail.com>
2024-02-12 14:19:57 +0100

    5th commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/', (req, res) =>{
 
 app.use(express.static("./client/build"))
 app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(port, () =>{
