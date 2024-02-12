30f2345ddf5f5104706fc107993c33ce4ef39dff
nikola <nicisekulovski@gmail.com>
2024-02-12 14:23:45 +0100

    5th commit

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-import express from 'express'
+import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv'
-import path from 'path'
-dotenv.config()
-const port = process.env.PORT || 3002
-const app = express()
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+dotenv.config();
 
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const port = process.env.PORT || 3002;
+const app = express();
 
 app.use(cors());
-app.post('/', (req, res) =>{
-    res.send("HELLO")
-})
 
-app.use(express.static("./client/build"))
-app.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+app.post('/', (req, res) => {
+    res.send("HELLO");
+});
 
-app.listen(port, () =>{
-console.log('Server is listening on port '+ `${port}` +'...')
+// Serve static files from the client build directory
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
 });
