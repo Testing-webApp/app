8d6889807986a982a0653663d845db4289d537f3
nikola <nicisekulovski@gmail.com>
2024-02-12 13:14:27 +0100

    sadf

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv'
 dotenv.config()
-const port = process.env.PORT || 3002
+const port = 80
 const app = express()
 
 
--- client/src/pages/baseUrl.js
+++ client/src/pages/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "http://localhost:3002";
+const baseUrl = "http://4.232.169.59:80";
 
 export default baseUrl;
