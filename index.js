b5c3e78761da69e9ebb07da4f2b05d7838722b59
nikola <nicisekulovski@gmail.com>
2024-02-14 23:06:50 +0100

    8th commit

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ app.post('/bla', (req, res) => {
 app.post('/login/Landlord', async(req, res)=>{
 
     
-    res.send("jebi ga")
-    /*
+    
     const {Email, Password} = req.body
     try{
     const data = await get_Landlord(Email);
@@ -73,7 +72,7 @@ app.post('/login/Landlord', async(req, res)=>{
     catch(e){
         res.send('The error is: ', e)
     }
-    */
+    
 
 });
 
